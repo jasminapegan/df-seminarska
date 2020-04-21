89cce6b351413041913d9a974968594386a0355c
sebastian <smeznar@gmail.com>
2020-04-21 19:02:00 +0200

    Implemented document digestion and updated the README

--- README.md
+++ README.md
@@ -7,11 +7,16 @@ Dodatna literatura (mogoče pride prav):
 TF-IDF: 
   - članek: https://pdfs.semanticscholar.org/b3bf/6373ff41a115197cb5b30e57830c16130c2c.pdf
   - http://www.tfidf.com/
+
 ssdeep: 
-  - člankek: https://www.dasec.h-da.de/wp-content/uploads/2012/08/2012_08_Breitinger_NeSeFo.pdf
+  - članek: https://www.dasec.h-da.de/wp-content/uploads/2012/08/2012_08_Breitinger_NeSeFo.pdf
   - https://ssdeep-project.github.io/ssdeep/index.html
+
 sdhash:
   - članek: https://hal.inria.fr/hal-01060620/document
   - https://github.com/sdhash/sdhash
-  dataset:
-  - http://roussev.net/t5/t5.html
+
+dataset: 
+- http://roussev.net/t5/t5.html
+
+Za pognat uporabi: `python fbhash.py pot/do/mape/ali/datoteke` ali v Pycharmu dodaj pot pod parametre.
--- code/fbhash.py
+++ code/fbhash.py
@@ -1,4 +1,37 @@
 import sys
+import os
+import glob
+import math
+from collections import Counter
+from itertools import chain
+
+
+def file_to_chunks(filename, a, n, k):
+    with open(filename, 'rb') as file:
+        rol_hash = 0
+        hashes = []
+        byte_table = ByteTable(k)
+        for i in range(k):
+            byte = file.read(1)
+            if not byte:
+                return hashes
+            num = int.from_bytes(byte, 'big')
+            byte_table.insert(num)
+            rol_hash = (a * rol_hash + num) % n
+        hashes.append(rol_hash)
+        ak = a ** k
+        while True:
+            byte = file.read(1)
+            if not byte:
+                return hashes
+            num = int.from_bytes(byte, 'big')
+            rol_hash = rolling_hash(rol_hash, byte_table.get(), num, a, ak, n)
+            byte_table.insert(num)
+            hashes.append(rol_hash)
+
+
+def rolling_hash(prev_hash, b0, bk, a, ak, n):
+    return (a * prev_hash - b0 * ak + bk) % n
 
 
 class ByteTable:
@@ -15,38 +48,57 @@ class ByteTable:
         return self.bt[self.p]
 
 
-def file_to_chunks(filename, a, n, k):
-    with open(filename, 'rb') as file:
-        rol_hash = 0
-        hashes = []
-        byte_table = ByteTable(k)
-        for i in range(k):
-            byte = file.read(1)
-            if not byte:
-                return hashes
-            num = int.from_bytes(byte, 'big')
-            byte_table.insert(num)
-            rol_hash = (a*rol_hash + num) % n
-        hashes.append(rol_hash)
-        ak = a**k
-        while True:
-            byte = file.read(1)
-            if not byte:
-                return hashes
-            num = int.from_bytes(byte, 'big')
-            rol_hash = rolling_hash(rol_hash, byte_table.get(), num, a, ak, n)
-            byte_table.insert(num)
-            hashes.append(rol_hash)
+class Document:
+    def __init__(self, filename: str):
+        self.params = DocParams()
+        chunks = file_to_chunks(filename, self.params.a, self.params.n, self.params.k)
+        self.chunk_freq = Counter(chunks)
+        self.weights = self.params.calculate_weights(self.chunk_freq, len(chunks))
+        self.digested_document = None
+
+    def document_terms(self):
+        return list(self.chunk_freq)
+
+    def digest(self, document_weights):
+        self.digested_document = {c: (self.weights[c]*document_weights[c]) for c in list(self.weights.keys())}
 
 
-def rolling_hash(prev_hash, b0, bk, a, ak, n):
-    return (a*prev_hash - b0*ak + bk) % n
+def create_database(folder_name: str):
+    file_names = [f for f in glob.glob(folder_name + "/**", recursive=True) if not os.path.isdir(f)]
+    documents = {f: Document(f) for f in file_names}
+    chunk_document_freq = Counter(chain.from_iterable([d.document_terms() for d in documents.values()]))
+    chunk_document_weight = document_weight(chunk_document_freq, len(file_names))
+    for d in documents.keys():
+        documents[d].digest(chunk_document_weight)
+    pass
+
+# Chunk frequency weighting functions inside the document
 
 
-def hash_a_file(filename):
-    chunk_hashes = file_to_chunks(filename, 2, 100, 7)
-    return 0
+def ch_tfidf_weight(element_freq: Counter, chunk_num: int):
+    return {c: (math.log2(1+element_freq[c]/chunk_num)) for c in list(element_freq)}
+
+
+def ch_freq_weight(element_freq: Counter, chunk_num: int):
+    return {c: (element_freq[c]/chunk_num) for c in list(element_freq)}
+
+# Document chunk frequency weighting functions
+
+
+def doc_log_weigh(document_freq: Counter, doc_num: int):
+    return {c: (math.log10(doc_num/document_freq[c])) for c in list(document_freq)}
+
+
+class DocParams:
+    def __init__(self):
+        self.k = 7  # from equation
+        self.n = 2793861040361076437  # random prime number 2^54 < n < 2^64
+        self.a = 97  # constant 0 < a < 256
+        self.calculate_weights = ch_freq_weight
+
+
+document_weight = doc_log_weigh
 
 
 if __name__ == '__main__':
-    hash_a_file(sys.argv[1])
+    create_database(sys.argv[1])
