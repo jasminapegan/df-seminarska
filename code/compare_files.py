431a0f37bb7cb3f6d3e3aa882d4a335c9cb58e33
Timotej Knez <tk7225@student.uni-lj.si>
2020-04-22 12:32:03 +0200

    Narejeno racunanje kosinusne razdalje

---
+++ code/compare_files.py
@@ -0,0 +1,34 @@
+import fbhash
+
+
+def cosine_distance(digested_document1, digested_document2):
+    keys_intersection = set(digested_document1.keys()) & set(digested_document2.keys())
+    scalar_product = 0
+    for key in keys_intersection:
+        scalar_product += digested_document1[key] * digested_document2[key]
+
+    vector_size1 = 0
+    for key in digested_document1.keys():
+        vector_size1 += digested_document1[key] ** 2
+    vector_size1 **= (1 / 2)
+
+    vector_size2 = 0
+    for key in digested_document2.keys():
+        vector_size2 += digested_document2[key] ** 2
+    vector_size2 **= (1 / 2)
+
+    return scalar_product / (vector_size1 * vector_size2)
+
+
+def comapre_files(corpus, filepath1, filepath2):
+    returned = fbhash.create_database(corpus)
+    documents = returned["documents"]
+    weights = returned["document_weights"]
+    file1 = fbhash.Document(filepath1)
+    file2 = fbhash.Document(filepath2)
+    file1.digest(weights)
+    file2.digest(weights)
+    return cosine_distance(file1.digested_document, file2.digested_document)
+
+if __name__ == '__main__':
+    print(comapre_files("../corpus", "../corpus/000060.html", "../corpus/000060.html"))
