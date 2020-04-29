ccdffaaace56732882c923069f569291cb076c8b
jasmy_peg@hotmail.com <jasmy_peg@hotmail.com>
2020-04-29 14:10:44 +0200

    napisala compare_zip_files, ne dela brisanje tmp datotek

--- code/compare_files.py
+++ code/compare_files.py
@@ -1,4 +1,7 @@
 import fbhash
+import os
+from zipfile import ZipFile
+from mimetypes import guess_type
 
 
 def cosine_distance(digested_document1, digested_document2):
@@ -37,5 +40,58 @@ def compare_files(corpus, filepath1, filepath2):
     file2.digest(cached_weights)
     return cosine_distance(file1.digested_document, file2.digested_document)
 
+
+def delete_dir(dir):
+    for item in os.listdir(dir):
+        if os.path.isdir(item):
+            delete_dir(item)
+        else:
+            os.remove(os.path.join(dir, item))
+    os.rmdir(dir)
+
+
+def generate_files_in_dir(dir, path=""):
+    for item in os.listdir(dir):
+        if os.path.isdir(item):
+            generate_files_in_dir(item, path=dir)
+        else:
+            yield os.path.join(path, dir, item)
+
+
+def compare_zip_files(corpus, filepath1, filepath2, tmpdir="tmp"):
+    cwd = os.getcwd()
+    root_dir = os.path.join(cwd, tmpdir)
+    tmpdir_f1, tmpdir_f2 = os.path.join(cwd, tmpdir, "f1"), os.path.join(cwd, tmpdir, "f2")
+    if not os.path.exists(root_dir):
+        os.mkdir(root_dir)
+    os.mkdir(tmpdir_f1)
+    os.mkdir(tmpdir_f2)
+
+    with ZipFile(filepath1, "r") as zf:
+        zf.extractall(tmpdir_f1)
+    with ZipFile(filepath2, "r") as zf:
+        zf.extractall(tmpdir_f2)
+
+    scores = []
+    for f1 in generate_files_in_dir(tmpdir_f1):
+        for f2 in generate_files_in_dir(tmpdir_f2):
+            print(f1 + ", " + f2 + "\n")
+            _, ext1 = os.path.splitext(f1)
+            _, ext2 = os.path.splitext(f2)
+            type = str(guess_type(f1))
+            print(str(type))
+            if ext1 == ext2 and ("image" in type or "text" in type):
+                scores.append(compare_files(corpus, f1, f2))
+
+    print(scores)
+    delete_dir(root_dir)
+    if scores:
+        return sum(scores) / len(scores)
+    else:
+        return 0.
+
+
 if __name__ == '__main__':
     print(compare_files("../corpus", "../corpus/000060.html", "../corpus/000060.html"))
+    # testni datoteki a.zip in b.zip, lahko se pobrišejo ko se ne rabjo več
+    # print(compare_zip_files("../corpus", "../corpus/a.zip", "../corpus/b.zip"))
